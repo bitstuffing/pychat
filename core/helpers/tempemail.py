374b2dcb711be493e309940e5823b47c315bcb42
bit <eduardo.higuero@hotmail.com>
2024-07-11 17:58:36 +0200

    some fixes related to six months of no time in life

--- core/helpers/tempemail.py
+++ core/helpers/tempemail.py
@@ -1,35 +1,55 @@
-from core.browser import Browser
 import requests
 
-class TempEmail(Browser):
-
+class TempEmail:
     def __init__(self):
-        #
-        self.session = requests.Session()
-        self.headers = {
-            'User-Agent': Browser.USER_AGENT,
+        self.email = None
+        self.token = None
+        self.messages = None  # Nuevo atributo para almacenar mensajes
+        self._base_url = 'https://api.internal.temp-mail.io/api/v3'
+        self._headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/115.0',
             'Accept': '*/*',
-            'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
-            'Referer': 'https://temp-mail.org/',
-            'Origin': 'https://temp-mail.org',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Referer': 'https://temp-mail.io/',
+            'Origin': 'https://temp-mail.io',
+            'DNT': '1',
             'Connection': 'keep-alive',
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Site': 'same-site',
+            'Sec-GPC': '1',
             'TE': 'trailers'
         }
-        response = self.session.post(url = 'https://web2.temp-mail.org/mailbox', headers = self.headers)
-        jsonResponse = response.json()
-        self.email = jsonResponse['mailbox'] # email
-        self.token = jsonResponse['token'] # jwt
+        self.fetch_new_email()
 
     def getEmail(self):
         return self.email
 
     def getEmails(self):
-        #curl 'https://web2.temp-mail.org/messages' --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0' -H 'Accept: */*' -H 'Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: gzip, deflate, br' -H 'Referer: https://temp-mail.org/' -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMzBiNDVkMmQwODRiNDA0OGE3MTZlYWNiNGZlNDZjYWUiLCJtYWlsYm94IjoibGFkZWxhMjQzNUBiaXRvZmVlLmNvbSIsImlhdCI6MTcwNjI5ODMxNn0.gsJCPMu_VmHn6XGI9zVeCXOxh4VFxZtd6tFlxBaHXGo' -H 'Origin: https://temp-mail.org' -H 'Connection: keep-alive' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-site' -H 'TE: trailers'
-        headers = self.headers
-        headers['Authorization'] = f'Bearer {self.token}'
-        response = self.session.get(url = 'https://web2.temp-mail.org/messages', headers = self.headers)
-        jsonResponse = response.json()
-        return jsonResponse['messages']
+        self.check_inbox()
+        return self.messages       
+
+    def fetch_new_email(self):
+        try:
+            response = requests.post(f'{self._base_url}/email/new', headers=self._headers)
+            response.raise_for_status()
+            data = response.json()
+            self.email = data.get('email')
+            self.token = data.get('token')
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching email: {e}")
+
+    def check_inbox(self):
+        try:
+            if not self.email:
+                print("Email address not fetched yet. Please fetch an email first.")
+                return
+            
+            url = f'{self._base_url}/email/{self.email}/messages'
+            response = requests.get(url, headers=self._headers)
+            response.raise_for_status()
+            print(response.text)
+            self.messages = response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching inbox messages: {e}")
--- main.py
+++
@@ -1,4 +0,0 @@
-from gui.main import main
-
-if __name__ == '__main__':
-	main()
